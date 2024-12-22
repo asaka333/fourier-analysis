8a7d64579615f9e67557797b9f58234df9d5aae7
asaka <hello.ryoasaka@gmail.com>
2024-12-22 12:58:38 +0900

    Add fourier_transform_1.ipynb

--- src.py
+++ src.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import numpy as np
 
 
@@ -21,15 +22,16 @@ def non_periodic_ex1(t):
 
 
 def non_periodic_ft_ex1(k):
-    if k == 0:
-        return None
-    else:
-        return 1 - np.exp(-1j * k) / (np.sqrt(2 * np.pi) * 1j * k)
+    return np.where(
+        k == 0,
+        np.nan,
+        (1 - np.exp(-1j * k)) / (np.sqrt(2 * np.pi) * 1j * k),
+    )
 
 
-def inv_fourier_transform(F, t, N, num_points=1000):
-    k = np.linspace(-N, N, num_points)
-    delta_k = 2 * N / num_points
+def inv_fourier_transform(F, t, N, num_points_per_one=10):
+    k = np.linspace(-N, N, num_points_per_one * N)
+    delta_k = 2 * N / num_points_per_one
 
     return (1 / np.sqrt(2 * np.pi)) * np.sum(F(k) * np.exp(1j * k * t) * delta_k)
 
@@ -42,33 +44,50 @@ def plot_periodic_ex2(ax, t=np.linspace(-4, 8, 1000)):
     return plot_function(ax, periodic_ex2, t)
 
 
-def plot_non_periodic_ex1(ax, t=np.linspace(-2, 6, 1000)):
+def plot_non_periodic_ex1(ax, t):
     return plot_function(ax, non_periodic_ex1, t)
 
 
-def plot_non_periodic_ft_ex1(ax, t):
+def plot_non_periodic_ft_ex1(
+    ax,
+    t,
+    range_color_red: Tuple[int, int] | None = None,
+):
+    return plot_function(ax, non_periodic_ft_ex1, t, range_color_red, False)
+
+
+def plot_non_periodic_invft_ex1(ax, N, t):
+
     return plot_function(
-        ax, non_periodic_ft_ex1, t=np.linspace(-15, 15, 1000), is_time_coordinate=False
+        ax,
+        lambda k: inv_fourier_transform(non_periodic_ft_ex1, k, N),
+        t,
+        is_time_coordinate=False,
     )
 
-def plot_invft_non_periodic_ft_ex1(ax, t=np.linspace(-2, 6, 1000)):
-    return  plot_function(
-        ax, inv_fourier_transform
-    )
 
-
-def plot_function(ax, periodic_func, t, is_time_coordinate=True):
+def plot_function(
+    ax,
+    periodic_func,
+    t,
+    range_color_red: Tuple[int, int] | None = None,
+    is_time_coordinate=True,
+):
     vectorized_f = np.vectorize(periodic_func)
     y = vectorized_f(t)
 
     ax.plot(t, y, drawstyle="steps-post")
-    ax.set_xlabel("$t$")
+    if range_color_red is not None:
+        mask = (t >= range_color_red[0]) & (t <= range_color_red[1])
+        ax.plot(t[mask], y[mask], drawstyle="steps-post", color="red")
     if is_time_coordinate:
         ax.set_ylabel("f(t)")
         ax.set_title("$f(t)$")
+        ax.set_xlabel("$t$")
     else:
         ax.set_ylabel("F(k)")
         ax.set_title("$F(k)$")
+        ax.set_xlabel("$k$")
     ax.axhline(0, color="black", linewidth=0.5, linestyle="--")
     ax.axvline(0, color="black", linewidth=0.5, linestyle="--")
     ax.grid(alpha=0.1)
